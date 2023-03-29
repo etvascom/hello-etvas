aeeb72a1ca08983c35d3c540db4ebc8214b21cf8
Castravet Radu-Mihai <31573322+devicedev@users.noreply.github.com>
2023-03-29 16:39:40 +0200

    feat(i18n): Pass storage to i18n to avoid using default session storage

--- client/src/shared/i18n/index.js
+++ client/src/shared/i18n/index.js
@@ -1,11 +1,13 @@
 import { I18nService } from '@etvas/i18n'
 import languages from './languages'
 import en from './dictionaries/en'
+import storage from '@shared/storage'
 
 export const i18nService = new I18nService(languages, {
   defaultLanguage: 'en',
   sessionKey: 'etvas.language',
-  dictionaryUrl: '/static/i18n/{lang}.json'
+  dictionaryUrl: '/static/i18n/{lang}.json',
+  storage,
 })
 
 i18nService.loadDictionary('en', en)
